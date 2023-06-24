5a78fd2c581ba19e0e0e570e0c73efd393ee70d1
Chukwuemeka Mba <76162087+Chukwuemeka-Mba@users.noreply.github.com>
2023-06-24 01:02:58 -0400

    completed OAuth flow

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    // const accessToken = urlParams.get("access_token");
+    const refreshToken = urlParams.get("refresh_token");
+
+    if (refreshToken) {
+      fetch(`/refresh_token?refresh_token=${refreshToken}`)
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img
+          src={
+            "https://i.scdn.co/image/ab6775700000ee855ed27974479c5f83d6bbf47e"
+          }
+          className="App-logo"
+          alt="logo"
+        />
+        <p></p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="http://localhost:8888/login"
           rel="noopener noreferrer"
         >
-          Learn React
+          Login to Spotify
         </a>
       </header>
     </div>
--- server/index.js
+++ server/index.js
@@ -7,10 +7,12 @@ const REDIRECT_URI =
   process.env.REDIRECT_URI || "http://localhost:8888/callback";
 
 const express = require("express");
+const cors = require("cors");
 const querystring = require("querystring");
 const axios = require("axios");
 const app = express();
 
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello, world! This is a new spotify app!");
 });
@@ -74,22 +76,23 @@ app.get("/callback", (req, res) => {
   })
     .then((response) => {
       if (response.status === 200) {
-        const { access_token, token_type } = response.data;
+        const { access_token, token_type, refresh_token } = response.data;
 
-        axios
-          .get("https://api.spotify.com/v1/me", {
-            headers: {
-              Authorization: `${token_type} ${access_token}`,
-            },
-          })
-          .then((response) => {
-            res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
-          })
-          .catch((err) => {
-            res.send(err);
-          });
+        const queryParams = querystring.stringify({
+          access_token,
+          refresh_token,
+        });
+
+        // Redirect to app
+        // pass along the  tokens in query params
+
+        res.redirect(`http://localhost:3000/?${queryParams}`);
       } else {
-        res.send(response);
+        res.redirect(
+          `/?${querystring.stringify({
+            error: "invalid_token",
+          })}`
+        );
       }
     })
     .catch((err) => {
@@ -98,12 +101,24 @@ app.get("/callback", (req, res) => {
 });
 
 app.get("/refresh_token", (req, res) => {
-  const { refresh_token } = res.data;
+  const { refresh_token } = req.query;
 
-  axios
-    .get(`http://localhost:8888/refresh_token?refresh_token=${refresh_token}`)
+  axios({
+    method: "post",
+    url: "https://accounts.spotify.com/api/token",
+    data: querystring.stringify({
+      grant_type: "refresh_token",
+      refresh_token: refresh_token,
+    }),
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${new Buffer.from(
+        `${CLIENT_ID}:${CLIENT_SECRET}`
+      ).toString("base64")}`,
+    },
+  })
     .then((response) => {
-      res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+      res.send(response.data);
     })
     .catch((err) => {
       res.send(err);
